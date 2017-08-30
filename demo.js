90e56b93b03f70f62ce37d5da9bd16acd36b3034
yong2.huang <yong2.huang@changhong.com>
2017-08-30 10:14:13 +0800

    spell

--- demo.js
+++ demo.js
@@ -2,6 +2,6 @@
  * Created by 栗子哥哥 on 2017/8/29.
  */
 var Sort =require('./lib/index.js')
-var Array =['iloveyou-7','iloveyou-1','iloveyou-3','iloveyou*4','iloveyou24',1,2,3]
-var result = Sort(Array);
+var Array =[{Nick:'iloveyou24'},{Nick:'iloveyou-7'},{Nick:'iloveyou-1'},{Nick:'iloveyou-3'},{Nick:'iloveyou*4'}]
+var result = Sort(Array,'Nick');
 console.log(result)
