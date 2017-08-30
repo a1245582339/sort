56a6aac0bd535138f257a2e98b29eabe2bca252f
yong2.huang <yong2.huang@changhong.com>
2017-08-30 09:36:48 +0800

    添加数组对象关键字排序

--- demo.js
+++ demo.js
@@ -1,10 +1,7 @@
 /**
  * Created by 栗子哥哥 on 2017/8/29.
  */
-var Sort =require('./index.js')
-
-
+var Sort =require('./lib/index.js')
 var Array =['iloveyou-7','iloveyou-1','iloveyou-3','iloveyou*4','iloveyou24',1,2,3]
-
 var result = Sort(Array);
 console.log(result)
--- sortmethod/spellsort.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by 栗子哥哥 on 2017/8/29.
- */
-
-
-module.exports = function (A,B,OPT) {
-   return A.localeCompare(B)
-}
