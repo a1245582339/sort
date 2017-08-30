d984952a271396f3b7b0ab6d21e4d7abe548b4b6
yong2.huang <yong2.huang@changhong.com>
2017-08-30 10:54:59 +0800

    sort.js

--- demo.js
+++ demo.js
@@ -2,6 +2,12 @@
  * Created by 栗子哥哥 on 2017/8/29.
  */
 var Sort =require('./lib/index.js')
-var Array =[{Nick:'iloveyou24'},{Nick:'iloveyou-7'},{Nick:'iloveyou-1'},{Nick:'iloveyou-3'},{Nick:'iloveyou*4'}]
+var Array =[
+    {Sn: 12,Nick:" 红外01"},
+    {Sn: 13,Nick:"B-DB-301(大会议室)"},
+    {Sn: 14,Nick:"B-DB-401(张总办公室)"},
+    {Sn: 15,Nick:'iloveyou22'},
+    {Sn: 16,Nick:'iloveyou*4'}];
+
 var result = Sort(Array,'Nick');
 console.log(result)
